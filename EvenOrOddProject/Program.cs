84d783b4212abb60d1b97ce7baf32a12d3f121f7
max-student <max-student@maxtrain.com>
2020-06-17 14:35:11 -0400

    Add project files.

---
+++ EvenOrOddProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EvenOrOddProject {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Enter a number... any number: ");
+
+            var ans = Console.ReadLine();
+            var nbr = Convert.ToInt32(ans);
+
+            if (nbr % 2 == 0) {
+                Console.WriteLine($"The number is even.");
+            } else {
+                Console.WriteLine($"The number is odd.");
+            }
+              
+        }
+    }
+}
